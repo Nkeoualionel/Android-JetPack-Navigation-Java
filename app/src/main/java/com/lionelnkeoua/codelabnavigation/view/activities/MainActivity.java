ca49ae357188294fbb2bd9ebbc46c4d63f88b3f6
lionel <nkeoualionel@gmail.com>
2020-07-29 08:22:59 +0200

    creating packages

--- app/src/main/java/com/lionelnkeoua/codelabnavigation/view/activities/MainActivity.java
+++ app/src/main/java/com/lionelnkeoua/codelabnavigation/view/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.lionelnkeoua.codelabnavigation;
+package com.lionelnkeoua.codelabnavigation.view.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.lionelnkeoua.codelabnavigation.R;
 
 public class MainActivity extends AppCompatActivity {
 
